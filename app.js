8ad3347048c09bd806c809b268a8ba98a86cc129
Alexis Santini <santini.alexis@gmail.com>
2017-07-15 23:32:04 +0200

    ~ cleaning

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const multer = require('multer');
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: '.env.example' });
+dotenv.load({ path: '.env' });
 
 /**
  * Controllers (route handlers).
